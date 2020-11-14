0f992fb7e6efa30224d0062d49d8aaf4df25323b
Peter <73221935+Rezistor-lab@users.noreply.github.com>
2020-11-15 00:58:23 +0100

    v1.3

--- CombatText/media/lua/client/CombatText.lua
+++ CombatText/media/lua/client/CombatText.lua
@@ -1,44 +1,45 @@
--- todo: zombies on fire are not showing dmg texts
-
-local print = print
-local context = {
+local CT = {
+	Fn = {
+		print = print,
+		pairs = pairs,
+		tostring = tostring,
+		getGametimeTimestamp = getGametimeTimestamp
+	},
+	FireDmgUpdate = 5,
 	TrackingList = {}, --list of monitored UID's
-	rgbMinus = {R=1.0F, G=0.4F, B=0.4F},
-	rgbPlus = {R=0.6F, G=1.0F, B=0.6F}
+	RgbMinus = {R=1.0D, G=0.4D, B=0.4D, A=1.0D},
+	RgbPlus = {R=0.6D, G=1.0D, B=0.6D, A=1.0D},
+	RgbOnFire = {R=0.8D, G=0.55D, B=0.0D, A=1.0D}
 }
 
-function len(tbl)
-  local count = 0
-  for _ in pairs(tbl) do count = count + 1 end
-  return count
+CT.Fn.damageDiff = function(currentHp, previousHp)
+	delta = (currentHp - previousHp)*100.0F
+	diff = ""
+	
+	if Math.abs(delta) < 5 then
+		diff = CT.Fn.tostring(luautils.round(delta,2))
+	elseif Math.abs(delta) < 10 then
+		diff = CT.Fn.tostring(luautils.round(delta,1))
+	else
+		diff = CT.Fn.tostring(luautils.round(delta,0))
+	end
+	
+	if currentHp > previousHp then 
+		return "+"..diff 
+	else
+		return diff
+	end
 end
 
-function updateCached(entity)
-	uid = entity:getUID()
+CT.Fn.getDamageColor = function(currentHp, previousHp, isOnFire)
+	if isOnFire then
+		return CT.RgbOnFire
+	end
 	
-	if context.TrackingList[uid] ~= nil then
-		if entity:isDead() == false then 
-			hpNow = entity:getHealth()
-			lastHP = contextTrackingList[uid]
-			if hpNow ~= lastHP then
-				diff = tostring(luautils.round((hpNow - lastHP)*100.0F,0))
-				colors = context.rgbMinus
-				if hpNow > lastHP then -- heal 
-					diff = '+'..diff
-					colors = context.rgbPlus
-					print(uid..' - lastHP:'..tostring(lastHP)..'|now:'..tostring(hpNow)..'|heal:'..diff)
-				else -- damage
-					print(uid..' - lastHP:'..tostring(lastHP)..'|now:'..tostring(hpNow)..'|dmg:'..diff)
-				end
-				
-				-- direct use of addLineChatElement instead of Say method to get around restrictions
-				-- params:text|R|G|B|font|?messageRadius?|type|allowBBcode|allowImages|allowChatIcons|allowColors|allowFonts|equalizeLineHeights
-				entity:addLineChatElement(diff, colors.R, colors.G, colors.B, UIFont.Medium, 90.0F, "radio", true,true,true,true,true,true)
-			end
-		else
-			print(uid..' - dead')
-		end
-		context.TrackingList[uid] = nil
+	if currentHp > previousHp then -- heal 
+		return CT.RgbPlus
+	else
+		return CT.RgbMinus
 	end
 end
 
@@ -50,66 +51,49 @@ function onHit(attacker, target, weapon, damage)
 	uid = target:getUID()
 	ttype = target:getObjectName()
 	
-	if ttype == "Zombie" or ttype == "Player" or ttype == "Survivor" then
-		if target:isDead() then
-			if context[ttype..'List'][uid] ~= nil then
-				context[ttype..'List'][uid] = nil
-				print(ttype..' - '..uid..' - removed from watchlist, reason:dead')
-			end
-		else
-			if context[ttype..'List'][uid] == nil then
-				context[ttype..'List'][uid] = target:getHealth()
-				print(ttype..' - '..uid..' - added to watchlist, reason:hit')
-			else
-				print(ttype..' - '..uid..' - still in watchlist, reason:hit')
-			end
-		end
-		print(ttype..' watch count:'..tostring(len(context[ttype..'List'])))
+	if (ttype == "Zombie" or ttype == "Player" or ttype == "Survivor" or ttype == "DeadBody") and CT.TrackingList[uid] == nil then
+		CT.TrackingList[uid] = { ["hp"] = target:getHealth(), ["entity"] = target, ["isOnFire"] = false, ["tick"] = CT.Fn.getGametimeTimestamp() }
 	end
 end
 
 --IsoZombie zombie
 function onZombieUpdate(zombie)
-	updateCached(zombie)
-end
---IsoZombie zombie
-function onZombieKilled(zombie)
-	updateCached(zombie)
-end
-
---IsoSurvivor survior
-function onSurviorUpdate(survior)
-	updateCached(survior)
+	uid = zombie:getUID()
+	if zombie:isOnFire() then
+		if CT.TrackingList[uid] == nil then
+			CT.TrackingList[uid] = { ["hp"] = zombie:getHealth(), ["entity"] = zombie, ["isOnFire"] = true, ["tick"]=CT.Fn.getGametimeTimestamp() }
+		elseif CT.TrackingList[uid].isOnFire ~= true then
+			CT.TrackingList[uid].isOnFire = true
+		end
+	elseif CT.TrackingList[uid] ~= nil and CT.TrackingList[uid].isOnFire == true then
+		CT.TrackingList[uid].isOnFire = false
+	end
 end
 
 --IsoPlayer player
 function onPlayerUpdate(player)
-	updateCached(player)
-end
---IsoPlayer player
-function onPlayerKilled(player)
-	updateCached(player)
-end
-
-
-function onBoot()
-	-- unregister first
-	Events.OnWeaponHitCharacter.Remove(onHit)
-	Events.OnZombieUpdate.Remove(onZombieUpdate)
-	Events.OnPlayerUpdate.Remove(onPlayerUpdate)
-	Events.OnNPCSurvivorUpdate.Remove(onSurviorUpdate)
-	Events.OnZombieDead.Remove(onZombieKilled)
-	Events.OnPlayerDeath.Remove(onPlayerKilled)
-	
-	-- register events
-	Events.OnWeaponHitCharacter.Add(onHit) --occurs before lowering target's health => store UID and current health which is used later in OnZombieUpdate|OnPlayerUpdate|OnNPCSurvivorUpdate events to determine "damage done"
-	Events.OnZombieUpdate.Add(onZombieUpdate) --occurs during every zombie update => check if we have tracking UID (we got hit) and show health change
-	Events.OnPlayerUpdate.Add(onPlayerUpdate) --occurs during every player update => check if we have tracking UID (we got hit) and show health change
-	Events.OnNPCSurvivorUpdate.Add(onSurviorUpdate) --occurs during every NPC update => check if we have tracking UID (we got hit) and show health change
-	Events.OnZombieDead.Add(onZombieKilled)
-	Events.OnPlayerDeath.Add(onPlayerKilled)
-	
-	print('GameBoot - event registration finished')
+	tick = CT.Fn.getGametimeTimestamp()
+	for uid, itm in CT.Fn.pairs(CT.TrackingList) do 
+		if itm ~= nil and ((itm.isOnFire and itm.tick + CT.FireDmgUpdate < tick) or itm.isOnFire == false) then
+			hpNow = itm.entity:getHealth()
+			if hpNow ~= itm.hp then
+				diff = CT.Fn.damageDiff(hpNow, itm.hp)
+				color = CT.Fn.getDamageColor(hpNow, itm.hp, itm.isOnFire)
+				
+				-- params:text|R|G|B|font|?messageRadius?|messageType|allowBBcode|allowImages|allowChatIcons|allowColors|allowFonts|equalizeLineHeights
+				itm.entity:addLineChatElement(diff, color.R, color.G, color.B, UIFont.Medium, 90.0F, "radio", true,true,true,true,true,true);
+				itm.hp = hpNow
+				itm.tick = tick
+			end
+			
+			if itm.entity:isDead() then 
+				CT.TrackingList[uid] = nil
+			end
+		end
+	end
 end
 
-Events.OnGameBoot.Add(onBoot)
+-- register events
+Events.OnWeaponHitCharacter.Add(onHit) --occurs before lowering target's health => store data in watchlist
+Events.OnZombieUpdate.Add(onZombieUpdate) --occurs during every zombie update => update isOnFire flag for burning damage indication
+Events.OnPlayerUpdate.Add(onPlayerUpdate) --occurs during every player update (much faster than OnZombieUpdate) => 
